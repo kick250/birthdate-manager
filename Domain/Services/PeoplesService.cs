58c2445854a510cf69ef27aadcbf750ad36a8aba
kick250 <kicklobal@gmail.com>
2022-11-29 21:49:41 -0300

    Criar tela de listagem de pessoas

---
+++ Domain/Services/PeoplesService.cs
@@ -0,0 +1,55 @@
+using System;
+using BirthdateManager.Models;
+using System.Collections.Generic;
+
+namespace BirthdateManager
+{
+  namespace Services
+  {
+    public class PeoplesService
+    {
+      private static List<People>? SavedPeoples = new List<People> {
+        new People("Breno", "Lobato", new DateTime(2002, 6, 28)),
+        new People("Joao", "Silva", new DateTime(2004, 12, 1)),
+        new People("Luana", "Silveira", new DateTime(2000, 1, 1)),
+      };
+
+      public static PeoplesService Build()
+      {
+        return new PeoplesService();
+      }
+
+      public List<People> GetAll()
+      {
+        if (SavedPeoples == null)
+          return new List<People> {};
+
+        return SavedPeoples;
+      }
+
+      public void Save(People people)
+      {
+        if (SavedPeoples == null)
+        {
+          SavedPeoples = new List<People> { people };
+          return;
+        }
+
+        SavedPeoples.Add(people);
+      }
+
+      public List<People> GetByName(string searchedName)
+      {
+        List<People> peoples = new List<People> {};
+
+        foreach (People people in GetAll())
+        {
+          string poepleName = people.GetFullName().ToLower();
+          if (poepleName.Contains(searchedName.ToLower()))
+            peoples.Add(people);
+        }
+        return peoples;
+      }
+    }
+  }
+}
--- WebApp/Controllers/PeoplesController.cs
+++ WebApp/Controllers/PeoplesController.cs
@@ -1,13 +1,19 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using BirthdateManager.Services;
+using BirthdateManager.Models;
 
 namespace WebApp.Controllers;
 
 public class PeoplesController : Controller
 {
+  PeoplesService peoplesService = PeoplesService.Build();
+
   public IActionResult Index()
   {
-    return View();
+    List<People> peoples = peoplesService.GetAll();
+
+    return View(peoples);
   }
 }
